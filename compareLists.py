32f7a5af495ac4f7e921df2641614a46ca186c19
Vincent <vincentsnow@gmx.com>
2017-08-01 13:34:55 -0700

    so easy.

---
+++ compareLists.py
@@ -0,0 +1,8 @@
+list_one = [1,2,3,4]
+list_two = [3,4,2,1]
+
+
+if (sorted(list_one) == sorted(list_two)):
+    print "These two lists are the same."
+else:
+    print "These two lists are different."
